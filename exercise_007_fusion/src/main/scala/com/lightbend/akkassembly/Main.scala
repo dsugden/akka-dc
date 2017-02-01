5be1e04f754ddc2ca463d0c26a997083e6eea2c4
Eric Loots <eric.loots@gmail.com>
2017-02-01 14:17:47 -0700

    Added materializer

--- exercise_007_fusion/src/main/scala/com/lightbend/akkassembly/Main.scala
+++ exercise_007_fusion/src/main/scala/com/lightbend/akkassembly/Main.scala
@@ -2,6 +2,8 @@ package com.lightbend.akkassembly
 
 import java.nio.file.FileSystems
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
 import akka.stream.alpakka.file.scaladsl.DirectoryChangesSource
 import akka.stream.scaladsl.Source
 
@@ -11,7 +13,11 @@ import scala.concurrent.duration._
 object Main {
   def main(args: Array[String]): Unit = {
 
-val path = args(0)
+    implicit val system = ActorSystem("akkassembly")
+    implicit val materializer = ActorMaterializer()
+    implicit val loggingAdapter = system.log
+    import system.dispatcher
+  val path = args(0)
 
     val fs = FileSystems.getDefault
     val changes = DirectoryChangesSource(fs.getPath(path), pollInterval = 1.second, maxBufferSize = 1000)
