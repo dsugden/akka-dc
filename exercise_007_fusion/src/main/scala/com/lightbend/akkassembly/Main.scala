17c55f8b55e2fa64d613104000343d39ae430037
dave <dave.sugden@gmail.com>
2017-02-01 16:10:07 -0500

    added sample code

--- exercise_007_fusion/src/main/scala/com/lightbend/akkassembly/Main.scala
+++ exercise_007_fusion/src/main/scala/com/lightbend/akkassembly/Main.scala
@@ -1,11 +1,24 @@
 package com.lightbend.akkassembly
 
+import java.nio.file.FileSystems
+
+import akka.stream.alpakka.file.scaladsl.DirectoryChangesSource
 import akka.stream.scaladsl.Source
 
 import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration._
 
 object Main {
   def main(args: Array[String]): Unit = {
+
+val path = args(0)
+
+    val fs = FileSystems.getDefault
+    val changes = DirectoryChangesSource(fs.getPath(path), pollInterval = 1.second, maxBufferSize = 1000)
+    changes.runForeach {
+      case (path, change) => println("Path: " + path + ", Change: " + change)
+    }
+
 
   }
 }
